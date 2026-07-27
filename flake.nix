762eed1910fd96d6a759fbd3ab5fd804c9b54ea5
Filip Riha <riha.fina@gmail.com>
2026-07-27 13:17:45 +0200

    Add rdevel

--- flake.nix
+++ flake.nix
@@ -50,6 +50,10 @@
           '';
         };
 
+        rdevel = {
+          path = ./rdevel;
+        };
+
         scala = {
           path = ./scala;
         };
---
+++ rdevel/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "R Development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-parts,
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "R";
+            packages = with pkgs; [
+              clang-tools
+            ];
+            inputsFrom = with pkgs; [
+              R
+            ];
+          };
+        };
+    };
+}
